b9def7b97b71e3c7577aee73653e5db5b0e725da
grosi <grosi.mail@gmail.com>
2013-04-28 19:33:23 +0200

    Sektor "Farben" hinzugefuegt

--- ContactManager/src/contactmanager/main/GraphicDesign.java
+++ ContactManager/src/contactmanager/main/GraphicDesign.java
@@ -4,10 +4,17 @@
  */
 package contactmanager.main;
 
+import java.awt.Color;
+
 /**
- *
+ * Globale Definitionen, die die UI beeinflussen
  * @author grosi
+ * @version 0.1
+ * @since 27.03.2013
  */
 public interface GraphicDesign {
+    
+    /** Farben */
+    public static final Color TAB_BACKGROUND_COLOR = new Color(179, 164, 164, 255);
     
 }
