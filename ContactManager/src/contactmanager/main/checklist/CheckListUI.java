27706020d9613efce97592c541e2e44456e80f93
grosi <grosi.mail@gmail.com>
2013-04-28 19:30:59 +0200

    Dynamische Liste fuer Label Eintraege

---
+++ ContactManager/src/contactmanager/main/checklist/CheckListUI.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contactmanager.main.checklist;
+
+import javax.swing.plaf.ComponentUI;
+
+/**
+ *
+ * @author grosi
+ */
+public abstract class CheckListUI extends ComponentUI{  
+}
---
+++ ContactManager/src/contactmanager/main/checklist/JCheckListException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contactmanager.main.checklist;
+
+/**
+ *
+ * @author grosi
+ */
+public class JCheckListException extends Exception{
+    
+    public JCheckListException(String s) {
+        super(s);
+    }
+    
+}
