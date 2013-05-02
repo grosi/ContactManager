1af34840e059da10ff36de116777067b6bb5a1a2
grosi <grosi.mail@gmail.com>
2013-05-02 09:22:58 +0200

    Head hinzugefuegt

--- ContactManager/src/contactmanager/main/groups/GroupsInterface.java
+++ ContactManager/src/contactmanager/main/groups/GroupsInterface.java
@@ -1,13 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package contactmanager.main.groups;
 
 /**
- *
- * @author grosi
+ * @author Simon Grossenbacher
+ * @version 0.1
+ * @since 02.05.2013
  */
-public class GroupsInterface {
+public interface GroupsInterface {
+    
+    /* Allgemeine Angaben */
+    public static final String GROUPS_TITLE = "Gruppen";
+    
+    /* Property Changes */
+    public static final String NAME_CHANGED_EVENT = "NAME_CHANGED"; 
     
 }
