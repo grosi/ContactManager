148eb41d93c29f89934c0c44130a187ac62dc451
grosi <grosi.mail@gmail.com>
2013-05-02 09:22:29 +0200

    Grund-Methoden fuer MVC implementiert

--- ContactManager/src/contactmanager/main/groups/GroupsModel.java
+++ ContactManager/src/contactmanager/main/groups/GroupsModel.java
@@ -1,13 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package contactmanager.main.groups;
 
+import contactmanager.main.AbstractModel;
+
 /**
- *
  * @author grosi
+ * @version 0.1
+ * @since 02.05.2013
  */
-public class GroupsModel {
+public class GroupsModel extends AbstractModel{
+    
+    private GroupsController controller;
+    private GroupsDAO groupsdao;
+    
+    public GroupsModel(GroupsController controller) {
+        super();
+        
+        this.controller = controller;
+        //this.groupsdao = controller.getMainController().getDAOFactory().getGroupsDAO();
+           
+    }
+
     
 }
