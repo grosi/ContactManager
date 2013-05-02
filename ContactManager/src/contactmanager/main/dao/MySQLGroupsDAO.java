3508fec859ddb849b641fb8389362d809d6ec0f9
grosi <grosi.mail@gmail.com>
2013-05-02 09:19:38 +0200

    ADD: Spezifische DAO fuer Gruppen-Modul

---
+++ ContactManager/src/contactmanager/main/dao/MySQLGroupsDAO.java
@@ -0,0 +1,16 @@
+
+package contactmanager.main.dao;
+
+import contactmanager.main.groups.GroupsDAO;
+
+/**
+ * @author grosi
+ * @version 0.1
+ * @since 02.05.2013
+ */
+public class MySQLGroupsDAO implements GroupsDAO {
+    
+    
+    public MySQLGroupsDAO() {}
+    
+}
