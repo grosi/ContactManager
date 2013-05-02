3aa9085d3713af517ab1597ff2524bee31c32967
grosi <grosi.mail@gmail.com>
2013-05-02 09:25:41 +0200

    DAO auskommentiert

--- ContactManager/src/contactmanager/main/contacts/ContactsModel.java
+++ ContactManager/src/contactmanager/main/contacts/ContactsModel.java
@@ -22,7 +22,7 @@ public final class ContactsModel extends AbstractModel {
         
         this.controller = controller;
         
-        this.contactsdao = controller.getMainController().getDAOFactory().getContactsDAO();
+        //this.contactsdao = controller.getMainController().getDAOFactory().getContactsDAO();
     }
     
     /**
