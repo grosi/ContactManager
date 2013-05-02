26dcb3a58f01f82eee136ff313d24d608b2dea23
grosi <grosi.mail@gmail.com>
2013-05-02 09:24:57 +0200

    Abstrakte GruppenDAO Methode hinzugefuegt

--- ContactManager/src/contactmanager/main/dao/DAOFactory.java
+++ ContactManager/src/contactmanager/main/dao/DAOFactory.java
@@ -2,6 +2,7 @@
 package contactmanager.main.dao;
 
 import contactmanager.main.contacts.ContactsDAO;
+import contactmanager.main.groups.GroupsDAO;
 
 /**
  * @author grosi
@@ -12,10 +13,19 @@ public abstract class DAOFactory {
     /* Liste mit allen verfuegbaren Daten */
     public static final int MySQL = 1;
     
+    /* Spezifische DAOs */
+    public abstract ContactsDAO getContactsDAO();
+    public abstract GroupsDAO getGroupsDAO();
+    
     protected DAOFactory() {}
     
-    public abstract ContactsDAO getContactsDAO();
-    
+   
+    /**
+     *
+     * @param database Art der Datenbank waehlen
+     * @return Datenbank Fabrik
+     * @throws DAOException
+     */
     public static DAOFactory getDAOFactory(int database) throws DAOException {
         switch (database) {
             case MySQL:
