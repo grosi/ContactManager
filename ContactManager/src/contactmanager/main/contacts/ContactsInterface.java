d9793b8a3a45353ae23b2a259c18a61447f0d0c3
grosi <grosi.mail@gmail.com>
2013-05-16 09:20:21 +0200

    EVENT-Bezeichnungen ergaenzt

--- ContactManager/src/contactmanager/main/contacts/ContactsInterface.java
+++ ContactManager/src/contactmanager/main/contacts/ContactsInterface.java
@@ -10,7 +10,13 @@ public interface ContactsInterface {
     public static final String CONTACTS_TITLE = "Kontakte";
     
     /* Property Changes */
-    public static final String NAME_CHANGED_EVENT = "NAME_CHANGED"; 
-    public static final String ADDRESS_CHANGED_EVENT = "ADDRESS_CHANGED";
-    
+    public static final String CONTACT_LIST_SELECT_EVENT = "CONTACT_LIST_SELECT";
+    public static final String CONTACT_SEARCH_EVENT = "CONTACT_SEARCH";
+    public static final String CONTACT_SELECT_EVENT = "CONTACT_SELECT";
+    public static final String CONTACT_INSERT_EVENT = "CONTACT_INSERT";
+    public static final String CONTACT_UPDATE_EVENT = "CONTACT_UPDATE";
+    public static final String CONTACT_DELETE_EVENT = "CONTACT_DELETE";
+    public static final String CONTACT_SELECT_CONTACTS_EVENT = "CONTACT_SELECT_GROUPS";
+    public static final String CONTACT_ADD_CONTACT_EVENT = "CONTACT_ADD_GROUP";
+    public static final String CONTACT_DELETE_CONTACT_EVENT = "CONTACT_DELETE_GROUP";    
 }
