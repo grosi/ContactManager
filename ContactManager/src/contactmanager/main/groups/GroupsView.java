25fc5dbe7df71b433d60f23013da9067f9d6ba8f
grosi <grosi.mail@gmail.com>
2013-05-16 09:24:26 +0200

    Demo Panel erstellt

--- ContactManager/src/contactmanager/main/groups/GroupsView.java
+++ ContactManager/src/contactmanager/main/groups/GroupsView.java
@@ -3,6 +3,7 @@ package contactmanager.main.groups;
 
 import contactmanager.main.AbstractView;
 import contactmanager.main.graphic.GraphicDesign;
+import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeEvent;
 
 /**
@@ -10,9 +11,20 @@ import java.beans.PropertyChangeEvent;
  * @version 0.1
  * @since 02.05.2013
  */
-public class GroupsView extends AbstractView implements GraphicDesign{
+public final class GroupsView extends AbstractView implements GraphicDesign, GroupsInterface{
     
     private GroupsController controller;
+    
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.ButtonGroup buttonGroup2;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JList jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextField jTextField1;
 
     
     /**
@@ -25,7 +37,7 @@ public class GroupsView extends AbstractView implements GraphicDesign{
         
         initComponents();
 
-        mainFrame.addTab(GroupsController.GROUPS_TITLE, this);
+        mainFrame.addTab(GROUPS_TITLE, this);
     }
     
     
@@ -33,14 +45,125 @@ public class GroupsView extends AbstractView implements GraphicDesign{
     
     @Override
     protected void initComponents() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    buttonGroup1 = new javax.swing.ButtonGroup();
+        buttonGroup2 = new javax.swing.ButtonGroup();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+
+        setMaximumSize(new java.awt.Dimension(66, 66));
+        setMinimumSize(new java.awt.Dimension(32, 32));
+        setPreferredSize(new java.awt.Dimension(800, 500));
+
+        jScrollPane1.setHorizontalScrollBarPolicy(javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jScrollPane1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        jScrollPane1.setMaximumSize(new java.awt.Dimension(500, 32767));
+        jScrollPane1.setMinimumSize(new java.awt.Dimension(200, 50));
+        jScrollPane1.setName(""); // NOI18N
+
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5dncjsdfczhsdbhjcbsdchjdbschjbdshjcbhdjbvchjdbvcjkhsdbvfhjd" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jList1.setMinimumSize(new java.awt.Dimension(150, 165));
+        jScrollPane1.setViewportView(jList1);
+
+        jTextField1.setText("jTextField1");
+
+        jButton1.setText("B1");
+        jButton1.setMaximumSize(new java.awt.Dimension(66, 66));
+        jButton1.setMinimumSize(new java.awt.Dimension(32, 32));
+        jButton1.setPreferredSize(new java.awt.Dimension(48, 48));
+        
+
+        jButton2.setText("B1");
+        jButton2.setMaximumSize(new java.awt.Dimension(66, 66));
+        jButton2.setMinimumSize(new java.awt.Dimension(32, 32));
+        jButton2.setPreferredSize(new java.awt.Dimension(48, 48));
+        
+
+        jButton3.setText("B1");
+        jButton3.setMaximumSize(new java.awt.Dimension(66, 66));
+        jButton3.setMinimumSize(new java.awt.Dimension(32, 32));
+        jButton3.setPreferredSize(new java.awt.Dimension(48, 48));
+        
+
+        jButton4.setText("B1");
+        jButton4.setMaximumSize(new java.awt.Dimension(66, 66));
+        jButton4.setMinimumSize(new java.awt.Dimension(32, 32));
+        jButton4.setPreferredSize(new java.awt.Dimension(48, 48));
+        
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 388, Short.MAX_VALUE)
+                    .addComponent(jTextField1))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 370, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE)
+                    .addComponent(jScrollPane2))
+                .addContainerGap())
+        );
+    }// </editor-fold>                        
+
+                                   
     
     
     /***************************************************************************
      * View -> Controller
      **************************************************************************/
     
+    private void addButtonActionPerformed(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private void removeButtonActionPerformed(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    private void saveButtonActionPerformed(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void messageButtonActionPerformed(ActionEvent evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
     
@@ -53,7 +176,37 @@ public class GroupsView extends AbstractView implements GraphicDesign{
      */
     @Override
     public void modelPropertyChange(PropertyChangeEvent evt) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        switch(evt.getPropertyName()) {
+            case GROUP_LIST_SELECT_EVENT:
+                break;
+                
+            case GROUP_SEARCH_EVENT:
+                break;
+                
+            case GROUP_SELECT_EVENT:
+                break;
+                
+            case GROUP_INSERT_EVENT:
+                break;
+                
+            case GROUP_UPDATE_EVENT:
+                break;
+                
+            case GROUP_DELETE_EVENT:
+                break;
+                
+            case GROUP_SELECT_CONTACTS_EVENT:
+                break;
+                
+            case GROUP_ADD_CONTACT_EVENT:
+                break;
+                
+            case GROUP_DELETE_CONTACT_EVENT:
+                break;
+                
+            default:
+                System.err.println("Unknows Event");
+        }
     }
     
 }
