c994b136186c18ba9157d18077cf57f5c0092e5f
grosi <grosi.mail@gmail.com>
2013-05-16 09:25:10 +0200

    EVENTs ergaenzt

--- ContactManager/src/contactmanager/main/groups/GroupsInterface.java
+++ ContactManager/src/contactmanager/main/groups/GroupsInterface.java
@@ -12,6 +12,13 @@ public interface GroupsInterface {
     public static final String GROUPS_TITLE = "Gruppen";
     
     /* Property Changes */
-    public static final String NAME_CHANGED_EVENT = "NAME_CHANGED"; 
-    
+    public static final String GROUP_LIST_SELECT_EVENT = "GROUP_LIST_SELECT";
+    public static final String GROUP_SEARCH_EVENT = "GROUP_SEARCH";
+    public static final String GROUP_SELECT_EVENT = "GROUP_SELECT";
+    public static final String GROUP_INSERT_EVENT = "GROUP_INSERT";
+    public static final String GROUP_UPDATE_EVENT = "GROUP_UPDATE";
+    public static final String GROUP_DELETE_EVENT = "GROUP_DELETE";
+    public static final String GROUP_SELECT_CONTACTS_EVENT = "GROUP_SELECT_CONTACTS";
+    public static final String GROUP_ADD_CONTACT_EVENT = "GROUP_ADD_CONTACT";
+    public static final String GROUP_DELETE_CONTACT_EVENT = "GROUP_DELETE_CONTACT";    
 }
