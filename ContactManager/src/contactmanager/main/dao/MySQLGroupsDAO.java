4edec4e6bfd841dda307b41bb9ed5f7d68060158
grosi <grosi.mail@gmail.com>
2013-05-16 09:22:44 +0200

    GroupDAO implementiert

--- ContactManager/src/contactmanager/main/dao/MySQLGroupsDAO.java
+++ ContactManager/src/contactmanager/main/dao/MySQLGroupsDAO.java
@@ -1,8 +1,10 @@
 
 package contactmanager.main.dao;
 
-import contactmanager.main.groups.Group;
+import contactmanager.main.contacts.ContactDTO;
+import contactmanager.main.groups.GroupDTO;
 import contactmanager.main.groups.GroupsDAO;
+import java.util.ArrayList;
 
 /**
  * @author grosi
@@ -15,18 +17,58 @@ public class MySQLGroupsDAO implements GroupsDAO {
     public MySQLGroupsDAO() {}
 
     @Override
-    public Group getGroup(int id) {
+    public ArrayList<GroupDTO> selectGroupList() throws DAOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean deleteGroup(int id) {
+    public ArrayList<GroupDTO> searchGroupList(String search_pattern) throws DAOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public boolean storeGroup(Group group) {
+    public ContactDTO selectGroup(int group_id) throws DAOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    @Override
+    public boolean insertGroup(ContactDTO insert_group) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateGroup(ContactDTO update_group) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteGroup(int group_id) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<ContactDTO> selectContactsFromGroup(int group_id) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addGroupToContact(int group_id, int contact_id) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteGroupFromContact(int group_id, int contact_id) throws DAOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+   
+
+
+    
+    
+
+    /***************************************************************************
+     * Private Methoden fuer Datenbankzugriff
+     **************************************************************************/
     
 }
