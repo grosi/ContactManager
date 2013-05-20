ed07523dec79cbd37d2df2f6f95d356943f8efd8
grosi <grosi.mail@gmail.com>
2013-05-20 21:28:34 +0200

    Konstanten hinzugefuegt

--- ContactManager/src/contactmanager/main/graphic/GraphicDesign.java
+++ ContactManager/src/contactmanager/main/graphic/GraphicDesign.java
@@ -5,6 +5,7 @@
 package contactmanager.main.graphic;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 /**
  * Globale Definitionen, die die UI beeinflussen
@@ -14,7 +15,62 @@ import java.awt.Color;
  */
 public interface GraphicDesign {
     
+    /** Bilder Dateipfad */
+    public static final String IMAGES_FILEPATH = "src/contactmanager/main/graphic/images/";
+    
     /** Farben */
     public static final Color TAB_BACKGROUND_COLOR = new Color(179, 164, 164, 255);
     
+    
+    /** Dimensionen */
+    /* Frame */
+    public static final int FRAME_MIN_WIDTH = 800;
+    public static final int FRAME_MIN_HEIGHT = 500;
+    
+    /* Gruppen Tab */
+    // Linke Spalte
+    public static final int GROUP_TAB_GROUPLIST_MIN_WIDTH = 250;
+    public static final int GROUP_TAB_GROUPLIST_MIN_HEIGHT = 200;
+    
+    //Rechte Spalte
+    public static final int GROUP_TAB_GROUPDETAIL_MIN_WIDTH = 250;
+    public static final int GROUP_TAB_GROUPDETAIL_MIN_HEIGHT = 200;
+    
+    
+    
+    /** Schaltflaechen Shortcuts */
+    
+    /* Kontake Tab */
+    
+    /* Gruppen Tab */
+    public static final int GROUP_TAB_MESSAGE_MNEMONIC = KeyEvent.VK_M;
+    public static final int GROUP_TAB_SAVE_MNEMONIC = KeyEvent.VK_S;
+    public static final int GROUP_TAB_ADD_MNEMONIC = KeyEvent.VK_A;
+    public static final int GROUP_TAB_REMOVE_MNEMONIC = KeyEvent.VK_R;
+    
+    
+    
+    /** Tool Tips */
+    
+    /* Gruppen Tab */
+    public static final String GROUP_TAB_MESSAGE_TOOLTIP = "E-Mail erstellen";
+    public static final String GROUP_TAB_SAVE_TOOLTIP = "Gruppe speichern";
+    public static final String GROUP_TAB_ADD_TOOLTIP = "Neue Gruppe erstellen";
+    public static final String GROUP_TAB_REMOVE_TOOLTIP = "Gruppe löschen";
+    
+    
+    
+    /** Labels */
+    
+    
+    /* Gruppen Tab */
+    public static final String GROUP_TAB_GROUPMEMBER_LABEL = "Gruppen-Mitglieder";
+    public static final String GROUP_TAB_GROUPOVERVIEW_LABEL = "Gruppen-Übersicht";
+    
+    
+    /** Textfelder */
+    
+    /* Gruppen Tab */
+    public static final String GROUP_TAB_DEFAULT_SEARCH_TEXT = "Suchbegriff eingeben...";
+    public static final String GROUP_TAB_DEFAULT_NAME_TEXT = "Gruppen-Name";
 }
