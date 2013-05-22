1bc6e7b546048cd48dd07df420d4bafd9d1d01af
grosi <grosi.mail@gmail.com>
2013-05-22 22:29:33 +0200

    Handler Methoden

--- ContactManager/src/contactmanager/main/groups/GroupsController.java
+++ ContactManager/src/contactmanager/main/groups/GroupsController.java
@@ -10,7 +10,7 @@ import contactmanager.main.frame.MainController;
  * @version 0.1
  * @since 02.05.2013
  */
-public class GroupsController extends AbstractController implements SubController {
+public final class GroupsController extends AbstractController implements SubController {
     
     private MainController main_controller;
 
@@ -26,13 +26,16 @@ public class GroupsController extends AbstractController implements SubControlle
         
         this.main_controller = mainController;
         
+        /* Views eintragen -> werden durch Modelle aktualisiert */
+        groups_view = new GroupsView(this);
+        addView(groups_view);
+        
         /* Modelle eintragen (diejenigen die Views aktualisieren sollen*/
         groups_model = new GroupsModel(this);
         addModel(groups_model);
         
-        /* Views eintragen -> werden durch Modelle aktualisiert */
-        groups_view = new GroupsView(this);
-        addView(groups_view);
+        /* Daten von Datenbank abfragen */
+        /** @TODO erste wenn DB funktioniert getGroupList(); */
     }
  
     
@@ -53,19 +56,53 @@ public class GroupsController extends AbstractController implements SubControlle
      **************************************************************************/
     
     public void getGroupList() {
-        
+        groups_model.getGroupList();
     }
+    
+    
+    /**
+     * Infromationen zu einer Gruppe
+     * @param group Gruppen Data Transfer Objekt
+     */
+    public void getGroup(GroupDTO group) {
+        groups_model.getGroup(group);
+    }
+    
     
     /**
      * Neue Gruppe hinzufuegen
      */
     public void addGroup() {
         groups_view.setDetailToDefault();
+        groups_view.deselectGroupList();
     }
     
+    
+    /**
+     * Gruppe loeschen
+     * @param group Gruppen Data Transfer Objekt
+     */
     public void removeGroup(GroupDTO group) {
         groups_model.removeGroup(group);
+        groups_view.deselectGroupList();
     }
+    
+    
+    /**
+     * Gruppe speichern
+     * @param group Gruppen Data Transfer Objekt
+     */
+    public void saveGroup(GroupDTO group) {
+        
+        /* Neue Gruppe erstellen */
+        if(group.group_id < 0)
+            groups_model.addGroup(group);
+        /* Gruppe aktualisieren */
+        else
+            groups_model.saveGroup(group);
+    }
+    
+    
     
 }
 
