a0c5df96261639f0557858b4519bfba614780acf
grosi <grosi.mail@gmail.com>
2013-05-22 22:28:22 +0200

    Methoden zum hinzufuegen, loeschen, aktualisieren und selektieren von Gruppen

--- ContactManager/src/contactmanager/main/groups/GroupsModel.java
+++ ContactManager/src/contactmanager/main/groups/GroupsModel.java
@@ -3,6 +3,7 @@ package contactmanager.main.groups;
 
 import contactmanager.main.AbstractModel;
 import contactmanager.main.dao.DAOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,7 +12,7 @@ import java.util.logging.Logger;
  * @version 0.1
  * @since 02.05.2013
  */
-public class GroupsModel extends AbstractModel implements GroupsInterface {
+public final class GroupsModel extends AbstractModel implements GroupsInterface {
     
     private GroupsController controller;
     private GroupsDAO groupsdao;
@@ -21,8 +22,56 @@ public class GroupsModel extends AbstractModel implements GroupsInterface {
         
         this.controller = controller;
         
-        //this.groupsdao = controller.getMainController().getDAOFactory().getGroupsDAO();
-           
+        //this.groupsdao = controller.getMainController().getDAOFactory().getGroupsDAO();   
+    }
+    
+    
+    /**
+     * Alle vorhanden Gruppen der Datenbank einlesen
+     */
+    public void getGroupList() {
+        
+        ArrayList<GroupDTO> group_list;
+        
+        try {
+            group_list = groupsdao.selectGroupList();
+        } catch (DAOException ex) {
+            group_list = null;
+        }
+        
+        firePropertyChange(GROUP_LIST_SELECT_EVENT, group_list, group_list);
+    }
+    
+    
+    /**
+     * Eine spezifische Gruppe der Datenbank einlesen
+     * @param group Gruppen Data Transfer Objekt
+     */
+    public void getGroup(GroupDTO group) {
+        
+        try {
+            group = groupsdao.selectGroup(group.group_id);
+        } catch (DAOException ex) {
+            group = null;
+        }
+        
+        firePropertyChange(GROUP_SELECT_EVENT, group, group);
+    }
+    
+    
+    /**
+     * Neue Gruppe zu Datenbank hinzufuegen
+     * @param group Gruppen Data Transfer Objekt
+     */
+    public void addGroup(GroupDTO group) {
+
+        try {
+            group.group_id = groupsdao.insertGroup(group);
+        } catch (DAOException ex) {
+            group = null;
+        }
+        
+        firePropertyChange(GROUP_INSERT_EVENT, group, group);
     }
     
     
@@ -39,6 +88,21 @@ public class GroupsModel extends AbstractModel implements GroupsInterface {
         }
         
         firePropertyChange(GROUP_DELETE_EVENT, group, group);    
+    }
+    
+    
+    /**
+     * Bestehende Gruppe in der Datenbank speichern (Aktualisieren)
+     * @param group
+     */
+    public void saveGroup(GroupDTO group) {
+        try {
+            groupsdao.updateGroup(group);
+        } catch (DAOException ex) {
+            group = null;
+        }
+        
+        firePropertyChange(GROUP_UPDATE_EVENT, group, group);
     }
 
     
