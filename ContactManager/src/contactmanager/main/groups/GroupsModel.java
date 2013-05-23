8219898df5847954f31b070af92b0a02635cb38b
grosi <grosi.mail@gmail.com>
2013-05-23 21:44:13 +0200

    Gruppen suchen Mail an Kontakte einer Gruppe senden

--- ContactManager/src/contactmanager/main/groups/GroupsModel.java
+++ ContactManager/src/contactmanager/main/groups/GroupsModel.java
@@ -2,6 +2,7 @@
 package contactmanager.main.groups;
 
 import contactmanager.main.AbstractModel;
+import contactmanager.main.contacts.ContactDTO;
 import contactmanager.main.dao.DAOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
@@ -29,8 +30,7 @@ public final class GroupsModel extends AbstractModel implements GroupsInterface 
     /**
      * Alle vorhanden Gruppen der Datenbank einlesen
      */
-    public void getGroupList() {
-        
+    public void getGroupList() { 
         ArrayList<GroupDTO> group_list;
         
         try {
@@ -48,7 +48,6 @@ public final class GroupsModel extends AbstractModel implements GroupsInterface 
      * @param group Gruppen Data Transfer Objekt
      */
     public void getGroup(GroupDTO group) {
-        
         try {
             group = groupsdao.selectGroup(group.group_id);
         } catch (DAOException ex) {
@@ -64,7 +63,6 @@ public final class GroupsModel extends AbstractModel implements GroupsInterface 
      * @param group Gruppen Data Transfer Objekt
      */
     public void addGroup(GroupDTO group) {
-
         try {
             group.group_id = groupsdao.insertGroup(group);
         } catch (DAOException ex) {
@@ -80,7 +78,6 @@ public final class GroupsModel extends AbstractModel implements GroupsInterface 
      * @param group Gruppen Data Transfer Objekt
      */
     public void removeGroup(GroupDTO group) {
-        
         try {
             groupsdao.deleteGroup(group.group_id);
         } catch (DAOException ex) {
@@ -93,7 +90,7 @@ public final class GroupsModel extends AbstractModel implements GroupsInterface 
     
     /**
      * Bestehende Gruppe in der Datenbank speichern (Aktualisieren)
-     * @param group
+     * @param group Gruppen Data Transfer Objekt
      */
     public void saveGroup(GroupDTO group) {
         try {
@@ -103,6 +100,45 @@ public final class GroupsModel extends AbstractModel implements GroupsInterface 
         }
         
         firePropertyChange(GROUP_UPDATE_EVENT, group, group);
+    }
+    
+    
+    /**
+     * Nach bestehenden Gruppen suchen, die zu dem angegeben Text passen
+     * @param text
+     */
+    public void searchGroup(String text) {
+        ArrayList<GroupDTO> group_list;
+        try {
+            group_list = groupsdao.searchGroupList(text);
+        } catch (DAOException ex) {
+            group_list = null;
+        }
+        
+        firePropertyChange(GROUP_SEARCH_EVENT, group_list, group_list);
+    }
+    
+    
+    /**
+     * E-Mail Adressen der Gruppen-Mitglieder einlesen
+     * @param group Gruppen Data Transfer Objekt
+     * @TODO E-Mail Prioritaeten beruecksichtigen
+     */
+    public void sendMessage(GroupDTO group) {
+        
+        try {
+            String email = null;
+            group = groupsdao.selectGroup(group.group_id);
+            
+            for(ContactDTO groupmember : group.group_contacts) {
+                if(groupmember.contact_email.get(0) != null)
+                    email += groupmember.contact_email.get(0).email_adress;
+            }
+            
+            controller.sendMessage(email);
+        } catch (DAOException ex) {
+            
+        }
     }
 
     
