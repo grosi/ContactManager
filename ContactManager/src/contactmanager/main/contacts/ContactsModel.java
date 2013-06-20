ee04fe1bbb98ce911b33f149afb22d4ad85d0451
Eder89 <ederp1@bluewin.ch>
2013-06-20 21:06:43 +0200

    Diverse Methoden

--- ContactManager/src/contactmanager/main/contacts/ContactsModel.java
+++ ContactManager/src/contactmanager/main/contacts/ContactsModel.java
@@ -54,4 +54,118 @@ public final class ContactsModel extends AbstractModel implements ContactsInterf
         firePropertyChange(CONTACT_SELECT_EVENT, contact, contact_db);
     }
     
+    
+    /**
+     * Neue Gruppe zu Datenbank hinzufuegen
+     * @param contact Gruppen Data Transfer Objekt
+     */
+    public void addContact(ContactDTO contact) {
+        try {
+            contact.user_id = contactsdao.insertContact(contact);
+//            if(contact.contact_adress.size() > 0)
+//                for(ContactDTO.ContactAdress address : contact.contact_adress)
+//                    contactsdao.insertContactAddress(contact.user_id,address);
+//            if(contact.contact_email.size() > 0)
+//                for(ContactDTO.ContactEmail email : contact.contact_email)
+//                    contactsdao.insertContactEmail(contact.user_id,email);
+//            if(contact.contact_phone.size() > 0)
+//                for(ContactDTO.ContactPhone phone : contact.contact_phone)
+//                    contactsdao.insertContactPhone(contact.user_id, phone);
+        } catch (DAOException ex) {
+            contact = null;
+        }
+        
+        firePropertyChange(CONTACT_INSERT_EVENT, null, contact);
+    }
+    
+    
+    /**
+     * Entfernt eine Gruppe
+     * @param group Gruppen Data Transfer Objekt
+     */
+    public void removeContact(ContactDTO contact) {
+        try {
+            contactsdao.deleteContact(contact.user_id);
+        } catch (DAOException ex) {
+            contact = null;
+        }
+        
+        firePropertyChange(CONTACT_DELETE_EVENT, null, contact);    
+    }
+    
+    
+    /**
+     * Bestehende Gruppe in der Datenbank speichern (Aktualisieren)
+     * @param group Gruppen Data Transfer Objekt
+     */
+    public void saveContact(ContactDTO contact) {
+        try {
+            contactsdao.updateContact(contact);
+//            if(contact.contact_adress.size() > 0) {
+//                for(ContactDTO.ContactAdress address : contact.contact_adress) {
+//                    if(address.adress_id != CONTACT_DEFAULT_ID)
+//                        contactsdao.updateContactAddress(address);
+//                    else
+//                        contactsdao.insertContactAddress(contact.user_id, address);
+//                }
+//            }
+//            if(contact.contact_email.size() > 0)
+//                for(ContactDTO.ContactEmail email : contact.contact_email) {
+//                    if(email.email_id != CONTACT_DEFAULT_ID)
+//                        contactsdao.updateContactEmail(email);
+//                    else
+//                        contactsdao.insertContactEmail(contact.user_id, email);
+//                }
+//            if(contact.contact_phone.size() > 0)
+//                for(ContactDTO.ContactPhone phone : contact.contact_phone) {
+//                    if(phone.phone_id != CONTACT_DEFAULT_ID)
+//                        contactsdao.updateContactPhone(phone);
+//                    else
+//                        contactsdao.insertContactPhone(contact.user_id, phone);
+//                }
+        } catch (DAOException ex) {
+            contact = null;
+        }
+        
+        firePropertyChange(CONTACT_UPDATE_EVENT, null, contact);
+    }
+    
+    
+    /**
+     * Nach bestehenden Gruppen suchen, die zu dem angegeben Text passen
+     * @param text
+     */
+    public void searchContact(String text) {
+        ArrayList<ContactDTO> contact_list;
+        try {
+            contact_list = contactsdao.searchContactList(text);
+        } catch (DAOException ex) {
+            contact_list = null;
+        }
+        
+        firePropertyChange(CONTACT_SEARCH_EVENT, null, contact_list);
+    }
+    
+    
+    /**
+     * E-Mail Adressen der Gruppen-Mitglieder einlesen
+     * @param group Gruppen Data Transfer Objekt
+     * @TODO E-Mail Prioritaeten beruecksichtigen
+     */
+    public void sendMessage(ContactDTO contact) {
+        String receiver = "";
+        ArrayList<ContactDTO.ContactEmail> email; 
+        
+//        try {
+//            email = groupsdao.selectEmailAddressFromGroup(contact.user_id);
+//            
+//            for(ContactDTO.ContactEmail email_adress : email)
+//                 receiver += email_adress.email_adress + ",";
+//            
+//            controller.sendEmail(receiver);
+//        } catch (DAOException ex) {
+//            
+//        }
+    }
+    
 }
