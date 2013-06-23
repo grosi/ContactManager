0061e0f3d5d07aa6c0f9ee62e473e4a38cf98791
Eder89 <ederp1@bluewin.ch>
2013-06-23 15:57:21 +0200

    Neuer event für Gruppenauslesen hinzugefügt

--- ContactManager/src/contactmanager/main/contacts/ContactsEvent.java
+++ ContactManager/src/contactmanager/main/contacts/ContactsEvent.java
@@ -20,5 +20,6 @@ public interface ContactsEvent {
     public static final String CONTACT_DELETE_EVENT = "CONTACT_DELETE";
     public static final String CONTACT_SELECT_GROUPS_EVENT = "CONTACT_SELECT_GROUPS";
     public static final String CONTACT_ADD_GROUP_EVENT = "CONTACT_ADD_GROUP";
-    public static final String CONTACT_DELETE_GROUP_EVENT = "CONTACT_DELETE_GROUP";    
+    public static final String CONTACT_DELETE_GROUP_EVENT = "CONTACT_DELETE_GROUP";
+    public static final String CONTACT_ALL_GROUP_EVENT = "CONTACT_ALL_GROUP";
 }
