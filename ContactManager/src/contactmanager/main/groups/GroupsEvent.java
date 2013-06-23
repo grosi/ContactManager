06d568e2500ed0acb85bdbc2c39badc757e8067a
grosi <grosi.mail@gmail.com>
2013-06-23 11:51:31 +0200

    Aufsplittung des Interfaces GroupsInterface in zwei Teile

--- ContactManager/src/contactmanager/main/groups/GroupsEvent.java
+++ ContactManager/src/contactmanager/main/groups/GroupsEvent.java
@@ -2,16 +2,23 @@
 package contactmanager.main.groups;
 
 /**
+ * Events des Gruppen Paketes
  * @author Simon Grossenbacher
  * @version 0.1
  * @since 02.05.2013
  */
-public interface GroupsInterface {
+public interface GroupsEvent {
     
     /* Allgemeine Angaben */
     public static final int GROUP_DEFAULT_ID = 0;
-    public static final String GROUPS_TITLE = "Gruppen";
+
     
+    /* Gruppen Handlen */
+    public static final String GROUP_ADD_GROUP = "ADD";
+    public static final String GROUP_REMOVE_GROUP_WITH_ID = "REMOVE_ID";
+    public static final String GROUP_REMOVE_GROUP_WITH_INDEX = "REMOVE_INDEX";
+
+
     /* Property Changes */
     public static final String GROUP_LIST_SELECT_EVENT = "GROUP_LIST_SELECT";
     public static final String GROUP_SEARCH_EVENT = "GROUP_SEARCH";
